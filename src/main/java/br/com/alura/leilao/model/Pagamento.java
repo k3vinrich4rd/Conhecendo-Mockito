b4ebe130cb064d6fd1b51eb2a3dc306579a7ec8b
k3vinrich4rd <kevinrichardve@gmail.com>
2023-06-12 10:04:34 -0300

    Inserindo lógica de teste para método estático

--- src/main/java/br/com/alura/leilao/model/Pagamento.java
+++ src/main/java/br/com/alura/leilao/model/Pagamento.java
@@ -1,17 +1,9 @@
 package br.com.alura.leilao.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.validation.constraints.NotNull;
 
 @Entity
 public class Pagamento {
