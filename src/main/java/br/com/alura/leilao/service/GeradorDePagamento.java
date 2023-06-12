b000a538ceccf035be61a66f4b73320bfe82ed56
k3vinrich4rd <kevinrichardve@gmail.com>
2023-06-12 10:03:33 -0300

    Inserindo lógica de teste para método estático

--- src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
+++ src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
@@ -6,20 +6,37 @@ import br.com.alura.leilao.model.Pagamento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 @Service
 public class GeradorDePagamento {
-    private PagamentoDao pagamentos;
+    private final PagamentoDao pagamentos;
+    private final Clock clock;
 
     @Autowired
-    public GeradorDePagamento(PagamentoDao pagamento) {
+    public GeradorDePagamento(PagamentoDao pagamento, Clock clock) {
         this.pagamentos = pagamento;
+        this.clock = clock;
     }
 
+    //Trecho de código suprimido
+
     public void gerarPagamento(Lance lanceVencedor) {
-        LocalDate vencimento = LocalDate.now().plusDays(1);
-        Pagamento pagamento = new Pagamento(lanceVencedor, vencimento);
+        LocalDate vencimento = LocalDate.now(clock).plusDays(1);
+        Pagamento pagamento = new Pagamento(lanceVencedor, proximoDiaUtil(vencimento));
         this.pagamentos.salvar(pagamento);
+    }
+
+    private LocalDate proximoDiaUtil(LocalDate dataBase) {
+        DayOfWeek diaDaSemana = dataBase.getDayOfWeek();
+        if (diaDaSemana == DayOfWeek.SATURDAY) {
+            return dataBase.plusDays(2);
+        } else if (diaDaSemana == DayOfWeek.SUNDAY) {
+            return dataBase.plusDays(1);
+        }
+
+        return dataBase;
     }
 }
--- src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -3,34 +3,61 @@ package br.com.alura.leilao.service;
 import br.com.alura.leilao.dao.PagamentoDao;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
 import br.com.alura.leilao.model.Usuario;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 
 import java.math.BigDecimal;
+import java.time.*;
 
 class GeradorDePagamentoTest {
 
     private GeradorDePagamento gerador;
+
     @Mock
     private PagamentoDao pagamentoDao;
+
+    //Para caputurar um determinado objeto
+    @Captor
+    private ArgumentCaptor<Pagamento> captor;
+
+    @Mock
+    private Clock clock;
 
     @BeforeEach
     public void inicializarMocks() {
         MockitoAnnotations.initMocks(this);
-        this.gerador = new GeradorDePagamento(pagamentoDao);
+        this.gerador = new GeradorDePagamento(pagamentoDao, clock);
+
     }
 
     @Test
     void deveriaCriarUmPagamentoParaVencedorLeilao() {
         Leilao leilaos = leiloes();
         Lance lanceVencedor = leilaos.getLanceVencedor();
+
+        ///Para testar o dia do pagamento
+        LocalDate data = LocalDate.of(2023, Month.JUNE, 7);
+        Instant instant = data.atStartOfDay(ZoneId.systemDefault()).toInstant();
+        Mockito.when(clock.instant()).thenReturn(instant);
+        Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+
         gerador.gerarPagamento(lanceVencedor);
+        //Esta verificando se o método foi chamado e tá capurando o parametro
+        Mockito.verify(pagamentoDao).salvar(captor.capture());
         //Para verificar um objeto criado
         //na classe que está sendo testada
         //Mockito.verify(pagamentoDao).salvar(Mockito.c);
+        //devolve o objeto capturado
+        Pagamento pagamento = captor.getValue();
+        Assertions.assertEquals(LocalDate.now(clock).plusDays(1), pagamento.getVencimento());
+        Assertions.assertEquals(lanceVencedor.getValor(), pagamento.getValor());
+        Assertions.assertFalse(pagamento.getPago());
+        Assertions.assertEquals(lanceVencedor.getUsuario(), pagamento.getUsuario());
+        Assertions.assertEquals(leilaos, pagamento.getLeilao());
     }
 
     private Leilao leiloes() {
